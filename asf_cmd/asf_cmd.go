185a8dbcb9a8a4341d3cac9d833f2efe072dd243
LMC <2_minchul@naver.com>
2020-08-26 01:04:56 +0900

    fix: fix exec

--- asf_cmd/asf_cmd.go
+++ asf_cmd/asf_cmd.go
@@ -7,12 +7,12 @@ import (
 
 func StartAsf() {
 	log.Println("start asf")
-	out, _ := exec.Command("systemctl start asf").CombinedOutput()
+	out, _ := exec.Command("systemctl", "start", "asf").CombinedOutput()
 	log.Println(string(out))
 }
 
 func StopAsf() {
 	log.Println("stop asf")
-	out, _ := exec.Command("systemctl stop asf").CombinedOutput()
+	out, _ := exec.Command("systemctl", "stop", "asf").CombinedOutput()
 	log.Println(string(out))
 }
